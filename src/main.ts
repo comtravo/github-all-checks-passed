de9688e26298c8516730c4863c2ae341fdd139fb
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-03-15 17:52:45 +0100

    ignore this check

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,6 @@ import {GitHub} from '@actions/github/lib/utils'
 
 type checksListForRefResponseType = Endpoints['GET /repos/{owner}/{repo}/commits/{ref}/check-runs']['response']
 type checkRunsType = Endpoints['GET /repos/{owner}/{repo}/commits/{ref}/check-runs']['response']['data']['check_runs']
-type createCheckResponseType = Endpoints['POST /repos/{owner}/{repo}/check-runs']['response']
-type updateCheckResponseType = Endpoints['PATCH /repos/{owner}/{repo}/check-runs/{check_run_id}']['response']
-
-const allChecksPassedActionMessage = 'All checks passed'
 
 function getOctokitClient(): InstanceType<typeof GitHub> {
   const token = process.env.GITHUB_TOKEN
@@ -28,67 +24,6 @@ async function getCheckRuns(
     repo: context.repo.repo,
     ref: context.sha
   })
-}
-
-async function setCheckRunResult(
-  octokit: InstanceType<typeof GitHub>,
-  thisCheckRunsId: number | undefined,
-  result: 'success' | 'failure'
-): Promise<createCheckResponseType | updateCheckResponseType> {
-  const summary =
-    result === 'success'
-      ? 'All checks passed'
-      : 'Some checks failed. check which check failed apart from this one'
-  if (!thisCheckRunsId) {
-    core.info('Creating a new check')
-    return octokit.checks.create({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      head_sha: context.sha,
-      name: allChecksPassedActionMessage,
-      status: 'completed',
-      conclusion: result,
-      output: {
-        title: 'Detail',
-        summary
-      }
-    })
-  } else {
-    core.info(`Updating the existing check with check id: ${thisCheckRunsId}`)
-    return octokit.checks.update({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      check_run_id: thisCheckRunsId,
-      status: 'completed',
-      conclusion: result,
-      output: {
-        title: 'Detail',
-        summary
-      }
-    })
-  }
-}
-
-function fetchCheckIdForThisAction(
-  res: checksListForRefResponseType
-): number | undefined {
-  const checkRuns = res.data.check_runs
-
-  const thisCheckRun = checkRuns.filter(
-    checkRun => checkRun.name === allChecksPassedActionMessage
-  )
-
-  if (thisCheckRun.length === 0) {
-    return
-  }
-
-  if (thisCheckRun.length > 1) {
-    throw new Error(
-      `More than 1 check run for this action: ${JSON.stringify(thisCheckRun)}`
-    )
-  }
-
-  return thisCheckRun[0].id
 }
 
 function findNonSuccessfulCheckRuns(
@@ -148,11 +83,9 @@ async function run(): Promise<void> {
     )
 
     if (someChecksArePending) {
-      core.info('Some checks are still pending')
+      core.warning('Some checks are still pending')
       return
     }
-
-    const checkIdOfThisCheckRun = fetchCheckIdForThisAction(checkRunsResponse)
 
     const nonSuccessfulRuns = findNonSuccessfulCheckRuns(
       checkRunsResponse,
@@ -161,8 +94,6 @@ async function run(): Promise<void> {
 
     if (!nonSuccessfulRuns || nonSuccessfulRuns.length === 0) {
       core.info('All checks passed')
-
-      await setCheckRunResult(octokit, checkIdOfThisCheckRun, 'success')
       return
     }
 
@@ -171,8 +102,7 @@ async function run(): Promise<void> {
         `${nonSuccessfulRun.name} failed to pass with conclusion ${nonSuccessfulRun.conclusion}`
       )
     }
-
-    await setCheckRunResult(octokit, checkIdOfThisCheckRun, 'failure')
+    core.setFailed(`Some checks failed`)
     return
   } catch (error) {
     core.setFailed(error.message)
